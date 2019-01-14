4b82d5de33d082641eaed6fb23238a5843ac1af6
EtiamNullam <swisrud+github@gmail.com>
2019-01-14 20:19:04 +0100

    Add FreeCamera mod

---
+++ src/FreeCamera/FreeCameraMod.cs
@@ -0,0 +1,16 @@
+﻿using Harmony;
+
+namespace FreeCamera
+{
+    /// <summary>
+    /// Stop constraining camera to the world.
+    /// </summary>
+    [HarmonyPatch(typeof(CameraController), "ConstrainToWorld")]
+    public static class FreeCameraMod
+    {
+        public static bool Prefix()
+        {
+            return false;
+        }
+    }
+}
