7a7df1b84a6df717228e686c4216dd56de3dc757
EtiamNullam <swisrud+github@gmail.com>
2019-01-19 00:29:45 +0100

    CustomTempOverlay: Remove HSV interp and tweak default values

--- src/CustomTemperatureOverlay/CustomTemperatureOverlayMod.cs
+++ src/CustomTemperatureOverlay/CustomTemperatureOverlayMod.cs
@@ -13,53 +13,6 @@ namespace CustomTemperatureOverlay
         // TODO: load config from file
         // TODO: set filewatcher on config
         // TODO: set keybind to set ranges to currently visible (adaptive overlay)
-        // TODO: benchmark vanilla temp overlay vs new
-
-        [HarmonyPatch(typeof(SimDebugView), nameof(SimDebugView.NormalizedTemperature))]
-        public static class SimDebugView_NormalizedTemperature
-        {
-            /// <summary>
-            /// Copied from decompiled code, just changed interpolation from RGB to HSV
-            /// </summary>
-            // TODO: use transpiler instead
-            public static bool Prefix(SimDebugView __instance, float temperature, ref Color __result)
-            {
-                int num = 0;
-                int num2 = 0;
-                for (int i = 0; i < __instance.temperatureThresholds.Length; i++)
-                {
-                    if (temperature <= __instance.temperatureThresholds[i].value)
-                    {
-                        num2 = i;
-                        break;
-                    }
-                    num = i;
-                    num2 = i;
-                }
-                float num3 = 0f;
-                if (num != num2)
-                {
-                    num3 = (temperature - __instance.temperatureThresholds[num].value) / (__instance.temperatureThresholds[num2].value - __instance.temperatureThresholds[num].value);
-                }
-                num3 = Mathf.Max(num3, 0f);
-                num3 = Mathf.Min(num3, 1f);
-
-                __result = LerpHSV(__instance.temperatureThresholds[num].color, __instance.temperatureThresholds[num2].color, num3);
-
-                return false;
-            }
-
-            private static ColorHSV LerpHSV(ColorHSV first, ColorHSV second, float factor)
-            {
-                return new ColorHSV
-                (
-                    Mathf.Lerp(first.H, second.H, factor),
-                    Mathf.Lerp(first.S, second.S, factor),
-                    Mathf.Lerp(first.V, second.V, factor),
-                    Mathf.Lerp(first.A, second.A, factor)
-                );
-            }
-        }
 
         [HarmonyPatch(typeof(SimDebugView), "OnPrefabInit")]
         public static class SimDebugView_OnPrefabInit
@@ -70,12 +23,12 @@ namespace CustomTemperatureOverlay
                 {
                     new SimDebugView.ColorThreshold // Exact Absolute Zero
                     {
-                        color = new Color(1,1,1,1),
+                        color = new Color(1,1,1,0.7f),
                         value = 0
                     },
                     new SimDebugView.ColorThreshold // Near Absolute Zero
                     {
-                        color = new Color(0.7f,0,1,1),
+                        color = new Color(0.65f,0,1,0.9f),
                         value = 5
                     },
                     new SimDebugView.ColorThreshold // Coldest Ice Biome
@@ -88,25 +41,25 @@ namespace CustomTemperatureOverlay
                         color = new Color(0,1,0,0.75f),
                         value = 273+20
                     },
-                    new SimDebugView.ColorThreshold // Spare2
+                    new SimDebugView.ColorThreshold // Warm
                     {
-                        color = new Color(1,0.75f,0,0.75f),
+                        color = new Color(0.9f,0.5f,0,0.75f),
                         value = 273+50
                     },
                     new SimDebugView.ColorThreshold // Hot Steam
                     {
-                        color = new Color(1,0,0,0.75f),
+                        color = new Color(0.9f,0,0,0.75f),
                         value = 273+125
-                    },
-                    new SimDebugView.ColorThreshold // Magma
-                    {
-                        color = new Color(1,0,0.35f,0.75f),
-                        value = 273+1250
                     },
                     new SimDebugView.ColorThreshold // Hot Magma
                     {
-                        color = new Color(1,0,0.7f,0.75f),
+                        color = new Color(1,0,0.35f,0.9f),
                         value = 273+2000
+                    },
+                    new SimDebugView.ColorThreshold // Spare
+                    {
+                        color = new Color(1,0,0.35f,0.9f),
+                        value = 273+2001
                     },
                 };
             }
