ca1fba935d503995523f42ec39aca5ac1b5dd243
EtiamNullam <swisrud+github@gmail.com>
2019-01-18 04:59:46 +0100

    Reduce chance of breaking at updates and do some cleanup

---
+++ src/SaveNoDailyReports/Patches/DisableValueReporting.cs
@@ -0,0 +1,18 @@
+﻿using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RemoveDailyReports.Patches
+{
+    [HarmonyPatch(typeof(ReportManager))]
+    [HarmonyPatch(nameof(ReportManager.ReportValue))]
+    public static class ReportManager_ReportValue_Disable
+    {
+        public static bool Prefix()
+        {
+            return false;
+        }
+    }
+}
---
+++ src/SaveNoDailyReports/Patches/NoReportsInReportManager.cs
@@ -0,0 +1,28 @@
+﻿using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RemoveDailyReports.Patches
+{
+    [HarmonyPatch(typeof(ReportScreen))]
+    [HarmonyPatch(nameof(ReportScreen.ShowReport))]
+    public static class ReportScreen_ShowReport_Disable
+    {
+        public static bool Prefix()
+        {
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(ReportScreen))]
+    [HarmonyPatch("Refresh")]
+    public static class ReportScreen_Refresh_Disable
+    {
+        public static bool Prefix()
+        {
+            return false;
+        }
+    }
+}
