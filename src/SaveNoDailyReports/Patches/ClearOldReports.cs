ffef179eeb066329c05f0548f346fa2a13035212
EtiamNullam <swisrud+github@gmail.com>
2019-01-18 19:27:10 +0100

    RemoveDailyReports: Remove only old reports and fix broken reports

---
+++ src/SaveNoDailyReports/Patches/ClearOldReports.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Harmony;
+
+namespace RemoveDailyReports.Patches
+{
+    [HarmonyPatch(typeof(ReportManager))]
+    [HarmonyPatch("OnNightTime")]
+    public static class ClearOldReports_AtNight
+    {
+        public static void Postfix(ref List<ReportManager.DailyReport> ___dailyReports)
+        {
+            int recentReportsToSpare = 5;
+            int dailyReportsCount = ___dailyReports.Count();
+
+            if (dailyReportsCount > recentReportsToSpare)
+            {
+                var reportsToClear = ___dailyReports.Take(dailyReportsCount - recentReportsToSpare);
+                foreach (var report in reportsToClear)
+                {
+                    report.reportEntries.Clear();
+                }
+            }
+        }
+    }
+}
---
+++ src/SaveNoDailyReports/Patches/FixMissingReports.cs
@@ -0,0 +1,47 @@
+﻿using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RemoveDailyReports.Patches
+{
+    // TODO: extract it to seperate mod?
+    public static class FixMissingReports
+    {
+        [HarmonyPatch(typeof(ReportManager))]
+        [HarmonyPatch("OnDeserialized")]
+        public static class ReportManager_FixMissingReports
+        {
+            public static void Postfix(ReportManager __instance, ref List<ReportManager.DailyReport> ___dailyReports)
+            {
+                try
+                {
+                    var missingCycles = GameUtil.GetCurrentCycle() - ___dailyReports.Count();
+
+                    if (missingCycles > 0)
+                    {
+                        var missingDailyReports = new ReportManager.DailyReport[missingCycles];
+
+                        for (int i = 0; i < missingCycles; i++)
+                        {
+                            var newDailyReport = new ReportManager.DailyReport(__instance)
+                            {
+                                day = i
+                            };
+                            newDailyReport.reportEntries.Clear();
+                            missingDailyReports[i] = newDailyReport;
+
+                        }
+                        ___dailyReports.InsertRange(0, missingDailyReports);
+                        Debug.Log("new reports count: " + ___dailyReports.Count());
+                    }
+                }
+                catch (Exception e)
+                {
+                    Debug.Log("RDR: Error while trying to fix missing daily reports: " + e);
+                }
+            }
+        }
+    }
+}
