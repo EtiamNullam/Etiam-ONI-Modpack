e4c375bfdd830491e865da16ac7366b47b436922
EtiamNullam <swisrud+github@gmail.com>
2019-01-21 20:23:49 +0100

    Add mod that clears regolith from storage lockers

---
+++ src/ClearRegolith/Patches/ClearRegolith.cs
@@ -0,0 +1,27 @@
+﻿using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace ClearRegolith
+{
+    [HarmonyPatch(typeof(Storage))]
+    [HarmonyPatch("OnSpawn")]
+    public static class ClearRegolith
+    {
+        public static void Postfix(Storage __instance)
+        {
+            foreach (GameObject gameObject in __instance.items)
+            {
+                PrimaryElement element = gameObject.GetComponent<PrimaryElement>();
+
+                if (element != null && element.ElementID == SimHashes.Regolith)
+                {
+                    element.Units /= 4;
+                }
+            }
+        } 
+    }
+}
