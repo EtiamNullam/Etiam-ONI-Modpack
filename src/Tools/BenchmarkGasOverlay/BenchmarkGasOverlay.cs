b6d969f0ad540008bead90805ab4d5390401c0a5
EtiamNullam <swisrud+github@gmail.com>
2019-01-23 00:47:34 +0100

    Improve results, reduce time and add mass range

--- src/Tools/BenchmarkGasOverlay/BenchmarkGasOverlay.cs
+++ src/Tools/BenchmarkGasOverlay/BenchmarkGasOverlay.cs
@@ -14,34 +14,51 @@ namespace BenchmarkGasOverlay
     {
         public static void Prefix()
         {
-            BenchmarkElement(SimHashes.CarbonDioxide);
-            BenchmarkElement(SimHashes.Oxygen);
-            BenchmarkElement(SimHashes.Hydrogen);
-            BenchmarkElement(SimHashes.SandStone);
+            BenchmarkCell(FindCell(SimHashes.CarbonDioxide, 1, 3), "CO2 no-pop");
+            BenchmarkCell(FindCell(SimHashes.CarbonDioxide, 6, 50), "CO2 pop");
+            BenchmarkCell(FindCell(SimHashes.Oxygen, 1, 3), "O2 no-pop");
+            BenchmarkCell(FindCell(SimHashes.SourGas, 6, 50), "SourGass pop");
+            BenchmarkCell(FindCell(SimHashes.SandStone), "Solid(Sandstone)");
         }
 
-        private static void BenchmarkElement(SimHashes element)
+        private static int FindCell(SimHashes material, float minMass = -1, float maxMass = -1)
         {
-            int benchmarkCell = -1;
+            int resultCell = -1;
             for (int i = 0; i < Grid.CellCount; i++)
             {
-                if (Grid.Element[i].id == element)
+                var element = Grid.Element[i];
+                var mass = Grid.Mass[i];
+
+                var validMinMass = minMass < 0 || mass > minMass;
+                var validMaxMass = maxMass < 0 || mass < maxMass;
+
+                if 
+                (
+                    element.id == material
+                    && validMinMass
+                    && validMaxMass
+                )
                 {
-                    benchmarkCell = i;
+                    resultCell = i;
                     break;
                 }
             }
 
-            if (benchmarkCell == -1)
+            if (resultCell == -1)
             {
-                Debug.Log("BenchmarkGasOverlay: Can't find gas cell on the map: " + Enum.GetName(typeof(SimHashes), element));
-                return;
+                Debug.Log($"BenchmarkGasOverlay: Can't find gas cell on the map: {Enum.GetName(typeof(SimHashes), material)}, in mass range: {minMass}-{maxMass}");
             }
 
+            return resultCell;
+        }
+
+        private static void BenchmarkCell(int cell, string message)
+        {
+
             var instance = SimDebugView.Instance;
-            int sample = 1000000;
-            int tries = 10;
-            var traverseMethod = Traverse.Create(instance).Method("GetOxygenMapColour", instance, benchmarkCell);
+            int sample = 100000;
+            int tries = 20;
+            var traverseMethod = Traverse.Create(instance).Method("GetOxygenMapColour", instance, cell);
             var results = new long[tries];
             var stopWatch = new Stopwatch();
 
@@ -59,15 +76,16 @@ namespace BenchmarkGasOverlay
 
             var result = new
             {
-                tries,
-                sample,
                 time_ms = new
                 {
                     min = results.Min(),
                     max = results.Max(),
                     average_ms = results.Average(),
+                    total = results.Sum()
                 },
-                element = Enum.GetName(typeof(SimHashes), Grid.Element[benchmarkCell].id)
+                tries,
+                sample,
+                message
             };
 
             Debug.Log("BenchmarkGasOverlay: result: " + JsonConvert.SerializeObject(result));
