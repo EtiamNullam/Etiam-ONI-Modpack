c0d8299b89187549a7d41627e60b9c2081c8a984
EtiamNullam <swisrud+github@gmail.com>
2019-01-26 02:31:12 +0100

    Stop discarding error code 0 from robocopy

--- src/MaterialColor/Patches/ApplyColors.cs
+++ src/MaterialColor/Patches/ApplyColors.cs
@@ -118,7 +118,6 @@ namespace MaterialColor.Patches
             }
         }
 
-        // TODO: run only if deconstructable is a tile
         [HarmonyPatch(typeof(Deconstructable), "OnCompleteWork")]
         public static class Deconstructable_OnCompleteWork_MatCol
         {
