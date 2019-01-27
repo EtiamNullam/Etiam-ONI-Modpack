431f36624fab63a8eb1f45f26298dd2d755326fe
EtiamNullam <swisrud+github@gmail.com>
2019-01-27 07:55:52 +0100

    Check logic on storage changes

--- src/SmartLockerDropPort/Patches/ConfigureSmartLocker.cs
+++ src/SmartLockerDropPort/Patches/ConfigureSmartLocker.cs
@@ -9,6 +9,8 @@ namespace SmartLockerDropPort.Patches
 {
     public static class SmartContainerConfig
     {
+        private static HashedString DropPortID = "LockerDropStorage";
+
         [HarmonyPatch(typeof(StorageLockerSmartConfig))]
         [HarmonyPatch(nameof(StorageLockerSmartConfig.DoPostConfigurePreview))]
         public class StorageLockerSmartConfig_DoPostConfigurePreview
@@ -43,30 +45,45 @@ namespace SmartLockerDropPort.Patches
         [HarmonyPatch("OnSpawn")]
         private static class StorageLockerSmart_OnSpawn
         {
-            public static void Postfix(StorageLockerSmart __instance, LogicPorts ___ports, FilteredStorage ___filteredStorage)
+            public static void Postfix(StorageLockerSmart __instance, LogicPorts ___ports)
             {
                 __instance.Subscribe(Convert.ToInt32(GameHashes.LogicEvent), logicValueChanged =>
                 {
                     var changed = (LogicValueChanged)logicValueChanged;
 
-                    if (changed.portID == DropPortID
-                        && changed.newValue == 1
-                        && ___ports.GetInputValue(DropPortID) == 1)
+                    if (changed.portID == DropPortID && changed.newValue == 1)
                     {
-                        var storage = Traverse.Create(___filteredStorage).Field<Storage>("storage").Value;
-                        storage.DropAll();
+                        CheckAndDrop(__instance, ___ports);
                     }
                 });
             }
         }
 
-        private static HashedString DropPortID = "LockerDropStorage";
+        [HarmonyPatch(typeof(StorageLockerSmart))]
+        [HarmonyPatch("UpdateLogicAndActiveState")]
+        private static class StorageLockerSmart_UpdateLogicAndActiveState
+        {
+            public static void Postfix(StorageLockerSmart __instance, LogicPorts ___ports)
+            {
+                CheckAndDrop(__instance, ___ports);
+            }
+        }
+
+        private static void CheckAndDrop(StorageLockerSmart storageLocker, LogicPorts ports)
+        {
+            if (ports.GetInputValue(DropPortID) == 1)
+            {
+                var filteredStorage = Traverse.Create(storageLocker).Field<FilteredStorage>("filteredStorage").Value;
+                var storage = Traverse.Create(filteredStorage).Field<Storage>("storage").Value;
+                storage.DropAll();
+            }
+        }
 
         private static void AddResetPort(GameObject gameObject, LogicPorts.Port defaultOutputPort)
         {
             var inputPorts = new[]
             {
-                new LogicPorts.Port(DropPortID, new CellOffset(0, 0), "Drops contents", true, LogicPortSpriteType.ResetUpdate)
+                new LogicPorts.Port(DropPortID, new CellOffset(0, 0), "Drops contents", true, LogicPortSpriteType.Input)
             };
 
             var outputPorts = new[]
