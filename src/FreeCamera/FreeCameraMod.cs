8dba3cff76426f4372c334a114db24ca24b773c0
EtiamNullam <swisrud+github@gmail.com>
2019-02-03 19:42:14 +0100

    Increase max zoom

--- src/FreeCamera/FreeCameraMod.cs
+++ src/FreeCamera/FreeCameraMod.cs
@@ -2,15 +2,46 @@
 
 namespace FreeCamera
 {
-    /// <summary>
-    /// Stop constraining camera to the world.
-    /// </summary>
-    [HarmonyPatch(typeof(CameraController), "ConstrainToWorld")]
-    public static class FreeCameraMod
+    public static class FreeCamera
     {
-        public static bool Prefix()
+        private const float MaxZoom = 350f;
+
+        /// <summary>
+        /// Stop constraining camera to the world.
+        /// </summary>
+        [HarmonyPatch(typeof(CameraController))]
+        [HarmonyPatch("ConstrainToWorld")]
+        public static class FreeTheCamera
         {
-            return false;
+            public static bool Prefix()
+            {
+                return false;
+            }
+        }
+
+        [HarmonyPatch(typeof(CameraController))]
+        [HarmonyPatch("OnPrefabInit")]
+        public static class CameraController_OnPrefabInit_SetMaxZoom
+        {
+            public static void Postfix(ref float ___maxOrthographicSize)
+            {
+                ___maxOrthographicSize = MaxZoom;
+            }
+        }
+
+        [HarmonyPatch(typeof(WattsonMessage))]
+        [HarmonyPatch("OnDeactivate")]
+        public static class WattsonMessage_OnDeactivate_SetMaxZoom_AfterDelay
+        {
+            public static void Postfix()
+            {
+                UIScheduler.Instance.Schedule
+                (
+                    "setZoomAfter_fadeInUI",
+                    3,
+                    data => CameraController.Instance.SetMaxOrthographicSize(MaxZoom)
+                );
+            }
         }
     }
 }
