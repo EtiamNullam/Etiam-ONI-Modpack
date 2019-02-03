4bd55a67e195a16f0c368298f4f4926760d19201
EtiamNullam <swisrud+github@gmail.com>
2019-02-03 19:50:41 +0100

    Improve structure

---
+++ src/FreeCamera/Patches/IncreaseMaxZoom.cs
@@ -0,0 +1,29 @@
+﻿using Harmony;
+
+namespace FreeCamera.Patches
+{
+    [HarmonyPatch(typeof(CameraController))]
+    [HarmonyPatch("OnPrefabInit")]
+    public static class CameraController_OnPrefabInit_SetMaxZoom
+    {
+        public static void Postfix(ref float ___maxOrthographicSize)
+        {
+            ___maxOrthographicSize = Config.MaxZoom;
+        }
+    }
+
+    [HarmonyPatch(typeof(WattsonMessage))]
+    [HarmonyPatch("OnDeactivate")]
+    public static class WattsonMessage_OnDeactivate_SetMaxZoom_AfterDelay
+    {
+        public static void Postfix()
+        {
+            UIScheduler.Instance.Schedule
+            (
+                "setZoomAfter_fadeInUI",
+                3,
+                data => CameraController.Instance.SetMaxOrthographicSize(Config.MaxZoom)
+            );
+        }
+    }
+}
