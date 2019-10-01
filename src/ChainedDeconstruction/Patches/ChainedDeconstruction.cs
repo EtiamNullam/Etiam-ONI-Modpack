143d60641e5c50abdcd0577eebdce9c292a0b360
EtiamNullam <swisrud+github@gmail.com>
2019-10-02 01:29:00 +0200

    Allow configuration of ChainedDeconstruction

--- src/ChainedDeconstruction/Patches/ChainedDeconstruction.cs
+++ src/ChainedDeconstruction/Patches/ChainedDeconstruction.cs
@@ -13,19 +13,45 @@ namespace ChainedDeconstruction
     [HarmonyPatch("TriggerDestroy")]
     public static class ChainedDeconstruction
     {
-        // TODO: move this to config
-        private static readonly string[] chainableBuildings = new string[]
-        {
-            "LadderComplete",
-            "LadderFastComplete",
-            "SteelLadderComplete",
-            "TravelTubeComplete",
-            "FirePoleComplete"
-        };
-
         private static readonly MethodInfo ForceDeconstruct = AccessTools.Method(typeof(Deconstructable), "OnCompleteWork");
         private static readonly object[] NullWorkerParameter = new[] { (Worker)null };
         private static readonly AccessTools.FieldRef<Deconstructable, bool> DestroyedGetter = AccessTools.FieldRefAccess<Deconstructable, bool>("destroyed");
+
+        private static readonly string ConfigFileName = "Chainables.json";
+
+        public static void OnLoad()
+        {
+            if (State.Common.ConfigPath == null)
+            {
+                return;
+            }
+
+            State.Common.WatchConfig<string[]>(ConfigFileName, SetChainables);
+
+            try
+            {
+                SetChainables(State.Common.LoadConfig<string[]>(ConfigFileName));
+            }
+            catch (Exception e)
+            {
+                State.Common.Logger.Log("Error while loading config.", e);
+            }
+        }
+
+        private static void SetChainables(string[] chainables)
+        {
+            if (chainables.Any(c => c == "*"))
+            {
+                State.ChainAll = true;
+            }
+            else
+            {
+                State.Chainables = chainables.Select(c => c + "Complete").ToArray();
+                State.ChainAll = false;
+            }
+
+            State.Common.Logger.Log("Set chainables");
+        }
 
         public static void Postfix(Deconstructable __instance, Building building)
         {
@@ -38,7 +64,12 @@ namespace ChainedDeconstruction
 
                 var name = __instance.name;
 
-                if (!DestroyedGetter(__instance) || !chainableBuildings.Contains(name))
+                if (
+                    DestroyedGetter(__instance) == false
+                    || (
+                        State.Chainables.Contains(name) == false
+                        && State.ChainAll == false)
+                    )
                 {
                     return;
                 }
---
+++ src/ChainedDeconstruction/State.cs
@@ -0,0 +1,24 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChainedDeconstruction
+{
+    public static class State
+    {
+        public static string[] Chainables = new string[]
+        {
+            "Ladder",
+            "LadderFast",
+            "SteelLadder",
+            "TravelTube",
+            "FirePole"
+        };
+
+        public static Core Common = new Core("ChainedDeconstruction", "1737893485", null, false);
+
+        public static bool ChainAll = false;
+    }
+}
