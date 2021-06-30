55c47cccf34e66515c557a491cce2b3678b2cf94
EtiamNullam <swisrud+git@gmail.com>
2021-06-30 14:29:32 +0200

    MaterialColor: Make config watch compliant with harmony2

--- src/MaterialColor/Patches/WatchConfig.cs
+++ src/MaterialColor/Patches/WatchConfig.cs
@@ -10,15 +10,17 @@ using System.Text;
 
 namespace MaterialColor.Patches
 {
-    public static class WatchConfig
+    public class WatchConfig : KMod.UserMod2
     {
         private static PeriodicRefresher Refresher;
 
         private static readonly string MainConfigFilename = "Config.json";
         private static readonly string ElementColorsFilename = "ElementColors.json";
 
-        public static void OnLoad()
+        public override void OnLoad(Harmony harmony)
         {
+            base.OnLoad(harmony);
+
             if (State.Common.ConfigPath == null)
             {
                 return;
