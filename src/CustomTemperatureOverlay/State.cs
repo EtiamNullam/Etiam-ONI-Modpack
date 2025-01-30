66550a5deea3d5d58959e18731c8052f98fc929f
EtiamNullam <10875340+EtiamNullam@users.noreply.github.com>
2025-01-30 18:44:03 +0200

    feature(custom-temperature-overlay): Improve colors, keep brightness more even, remove transparency

--- src/CustomTemperatureOverlay/State.cs
+++ src/CustomTemperatureOverlay/State.cs
@@ -18,42 +18,42 @@ namespace CustomTemperatureOverlay
         {
             new TemperatureStep // Exact Absolute Zero
             (
-                new Color(1,1,1,0.85f),
+                new Color(1, 1, 1, 1),
                 0
             ),
             new TemperatureStep // Near Absolute Zero
             (
-                new Color(0.35f,0,1,0.9f),
+                new Color(0.35f, 0, 1, 1),
                 5
             ),
             new TemperatureStep // Coldest Ice Biome
             (
-                new Color(0,0,1,0.8f),
+                new Color(0.1f, 0.1f, 1, 1),
                 273-60
             ),
             new TemperatureStep // Temperate
             (
-                new Color(0.2f,1,0,0.8f),
+                new Color(0, 1, 0, 1),
                 273+20
             ),
             new TemperatureStep // Warm
             (
-                new Color(1,1,0,0.85f),
+                new Color(1, 1, 0, 1),
                 273+35
             ),
             new TemperatureStep // Hot
             (
-                new Color(1,0.5f,0,0.75f),
+                new Color(1, 0.5f, 0, 1),
                 273+60
             ),
             new TemperatureStep // Hot Steam
             (
-                new Color(0.9f,0,0,0.85f),
+                new Color(0.9f, 0, 0, 1),
                 273+125
             ),
             new TemperatureStep // Hot Magma
             (
-                new Color(1,0,0.35f,0.9f),
+                new Color(1, 0.1f, 0.45f, 1),
                 273+2000
             ),
         };
